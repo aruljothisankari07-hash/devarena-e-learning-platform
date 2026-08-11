0fd9716ebb0a994fb4f05be62a3789fa2eefaaf1
aruljothisankari07-hash <aruljothisankari07@gmail.com>
2026-08-11 20:29:38 +0530

    Add files via upload

---
+++ authRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  registerUser,
+  loginUser,
+  sendOTP,
+  verifyOTP,
+} = require("../controllers/authController");
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.post("/send-otp", sendOTP);
+
+router.post("/verify-otp", verifyOTP);
+
+module.exports = router;
