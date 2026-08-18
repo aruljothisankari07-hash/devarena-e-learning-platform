efd26d27650447678bdede6989dd80811355877c
YOUR_GITHUB_USERNAME <aruljothisankari07@gmail.com>
2026-08-18 19:46:21 +0530

    Update backend API URL

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://devarena-e-learning-platform.onrender.com/api",
 });
 
 export default API;
